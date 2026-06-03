913ce04af068316a9f0ad13a90ae9943416beb53
Shikhar Maurya <111562809+LexonPro@users.noreply.github.com>
2026-06-03 18:55:46 +0530

    Implement moveZeroes function to shift zeros

---
+++ Week_1/Day_3/moveZeros.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int count = 0;
+        vector<int> temp;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != 0)
+                temp.push_back(nums[i]);
+            else
+                count++;
+        }
+
+        while(count--) {
+            temp.push_back(0);
+        }
+
+        nums = temp;
+    }
+};
